25ef6eed10b95c642d0a77c304bbd68322011698
solomonbaez <solomon.james.baez@gmail.com>
2023-09-08 12:27:17 -0700

    Fixed slight typo in create-tables.sql

--- go_databases/data-access/create-tables.sql
+++ go_databases/data-access/create-tables.sql
@@ -3,11 +3,11 @@ CREATE TABLE album (
   id         INT AUTO_INCREMENT NOT NULL,
   title      VARCHAR(128) NOT NULL,
   artist     VARCHAR(255) NOT NULL,
-  price      DECIMALS(5,2) NOT NULL,
+  price      DECIMAL(5,2) NOT NULL,
   PRIMARY KEY (`id`)
-)
+);
 
-INSERT INTO album 
+INSERT INTO album
   (title, artist, price)
 VALUES
   ("The Beach", "Amiture", 9.00),
