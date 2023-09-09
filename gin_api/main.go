8ced332dec121af291d63f1b61ac703ccab7bed6
solomonbaez <solomon.james.baez@gmail.com>
2023-09-09 11:06:59 -0700

    Sketched out DB representation variable

--- gin_api/main.go
+++ gin_api/main.go
@@ -9,6 +9,12 @@ type Album struct {
 	Price  float64 `json:"price"`
 }
 
+var albums = []Album{
+	{ID: "1", Title: "The Beach", Artist: "Amiture", Price: 9.00},
+	{ID: "2", Title: "Careful", Artist: "Boy Harsher", Price: 8.00},
+	{ID: "3", Title: "Visions", Artist: "Grimes", Price: 14.99},
+}
+
 func main() {
-	fmt.Println("Initialized!")
+	fmt.Println(albums)
 }
