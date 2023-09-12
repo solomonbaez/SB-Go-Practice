6b1ec4d6f5220a1e39deb0af041d72aa7c931bf4
solomonbaez <solomon.james.baez@gmail.com>
2023-09-11 18:38:03 -0700

    Finalized fuzzable function

--- go_fuzzing/reverse_test.go
+++ go_fuzzing/reverse_test.go
@@ -5,23 +5,6 @@ import (
 	"unicode/utf8"
 )
 
-func TestReverse(t *testing.T) {
-	testCases := []struct {
-		in, want string
-	}{
-		{"Hello, world", "dlrow ,olleH"},
-		{"!12345", "54321!"},
-		{" ", " "},
-	}
-
-	for _, testCase := range testCases {
-		reversedString := StringReverse(testCase.in)
-		if reversedString != testCase.want {
-			t.Errorf("Reverse: %v, want %v", reversedString, testCase.want)
-		}
-	}
-}
-
 func FuzzReverse(f *testing.F) {
 	testCases := []string{"Hello, world", " ", "!12345"}
 	for _, testCase := range testCases {
@@ -29,13 +12,14 @@ func FuzzReverse(f *testing.F) {
 	}
 
 	f.Fuzz(func(t *testing.T, original string) {
-		singleReverse := StringReverse(original)
-		doubleReverse := StringReverse(singleReverse)
-		t.Logf("Number of runes: original=%d, reverse=%d, double-reverse=%d",
-			utf8.RuneCountInString(original),
-			utf8.RuneCountInString(singleReverse),
-			utf8.RuneCountInString(doubleReverse),
-		)
+		singleReverse, e_1 := StringReverse(original)
+		if e_1 != nil {
+			return
+		}
+		doubleReverse, e_2 := StringReverse(singleReverse)
+		if e_2 != nil {
+			return
+		}
 
 		if original != doubleReverse {
 			t.Errorf("Before: %v, after: %v", original, doubleReverse)
